ae904fe283b7a9c20916708959bc12f8ae905dd2
annguyen1511 <annguyen1511>
2022-05-31 02:53:02 +0700

    add local motive

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/style.scss'
+import { useEffect } from "react";
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    let scroll;
+    import("locomotive-scroll").then((locomotiveModule) => {
+      scroll = new locomotiveModule.default({
+        el: document.querySelector("[data-scroll-container]"),
+        smooth: true,
+        smoothMobile: false,
+        resetNativeScroll: true
+      });
+    });
+
+    // `useEffect`'s cleanup phase
+    return () => scroll.destroy();
+  });
+
+  return (
+    <main className="main" data-scroll-container>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
