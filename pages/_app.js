8a471c334636d089cf2a582fd2f1b31e742d2b41
annguyen1511 <annguyen1511>
2022-05-31 09:29:54 +0700

    remove local motive

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,29 @@
 import '../styles/style.scss'
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    let scroll;
-    import("locomotive-scroll").then((locomotiveModule) => {
-      scroll = new locomotiveModule.default({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        smoothMobile: false,
-        resetNativeScroll: true
-      });
-    });
+  // useEffect(() => {
+  //   let scroll;
+  //   import("locomotive-scroll").then((locomotiveModule) => {
+  //     scroll = new locomotiveModule.default({
+  //       el: document.querySelector("[data-scroll-container]"),
+  //       smooth: true,
+  //       smoothMobile: false,
+  //       resetNativeScroll: false
+  //     });
+  //   });
 
-    // `useEffect`'s cleanup phase
-    return () => scroll.destroy();
-  });
+  //   // `useEffect`'s cleanup phase
+  //   return () => scroll.destroy();
+  // });
 
   return (
-    <main className="main" data-scroll-container>
-      <Component {...pageProps} />
-    </main>
+    // <main data-scroll-container>
+    //   <Component  {...pageProps} />
+    // </main>
+    <Component  {...pageProps} />
   )
 }
 
